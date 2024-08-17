28db7147fd8c3cd683eb747ec79f64a6069eb681
Prathamesh <pdpatil1006@gmail.com>
2024-08-17 19:20:21 +0530

    Create findRepetitiveElmt.java

---
+++ Assignmets/src/Assignment18/findRepetitiveElmt.java
@@ -0,0 +1,50 @@
+/*Q3.Given an array of size N filled with numbers from 1 to N-1 in random order. The array has only one repetitive element. The task is to find the repetitive element.
+
+Examples:
+
+Input: a[] = {1, 3, 2, 3, 4}
+Output: 3*/
+package Assignment18;
+
+import java.util.Scanner;
+
+public class findRepetitiveElmt {
+
+    int a[];
+    findRepetitiveElmt(int b[]){
+        a=b;
+    }
+    public void findElmt(){
+        int c=1;
+        for(int i=0;i<a.length;i++){
+            c=1;
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]==a[j]){
+                    c++;
+                    a[j]=-1;
+                }
+            }
+            if(c>1 && a[i]!=-1){
+                System.out.print("Most repetitive element is: "+a[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your array size: ");
+        int s = sc.nextInt();
+        int a[] = new int[s];
+        System.out.print("Enter your array element: ");
+        for (int i=0;i<s;i++){
+            a[i]=sc.nextInt();
+        }
+        findRepetitiveElmt ob= new findRepetitiveElmt(a);
+        ob.findElmt();
+    }
+}
+/******************************OUTPUT********************************
+ Enter your array size: 5
+ Enter your array element: 1 3 2 3 4
+ Most repetitive element is: 3
+ *******************************************************************/
