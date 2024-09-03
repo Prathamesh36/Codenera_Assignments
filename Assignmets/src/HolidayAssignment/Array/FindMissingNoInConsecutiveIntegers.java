e6beb90ebd7488525d577d2e6252b1a6deed4329
Prathamesh <pdpatil1006@gmail.com>
2024-09-03 23:13:39 +0530

    v38

---
+++ Assignmets/src/HolidayAssignment/Array/FindMissingNoInConsecutiveIntegers.java
@@ -0,0 +1,32 @@
+/*50.	Write a program to find the missing number in an array of consecutive integers.*/
+package HolidayAssignment.Array;
+
+import java.util.Scanner;
+
+public class FindMissingNoInConsecutiveIntegers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array size: ");
+        int s= sc.nextInt();
+        int []a = new int[s];
+        System.out.print("Enter Array element: ");
+        for(int i=0;i<s;i++){
+            a[i]=sc.nextInt();
+        }
+        int sum =0;
+        for (int i = a[0]; i <=a[a.length-1] ; i++) {
+            sum+=i;
+        }
+        int total_sum=0;
+        for (int i = 0; i<a.length ; i++) {
+            total_sum+=a[i];
+        }
+        int miss=sum-total_sum;
+        System.out.print("Missing number is: "+miss);
+    }
+}
+/************************OUTPUT****************************
+ Enter Array size: 5
+ Enter Array element: 1 2 3 4 6
+ Missing number is: 5
+ *********************************************************/
---
+++ Assignmets/src/HolidayAssignment/Array/FindSpecificElmt.java
@@ -0,0 +1,37 @@
+/*48.	Write a program to check if an array contains a specific element.*/
+package HolidayAssignment.Array;
+
+import java.util.Scanner;
+
+public class FindSpecificElmt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your array size: ");
+        int s = sc.nextInt();
+        int a[] = new int[s];
+        System.out.print("Enter Array Elemets: ");
+        for(int i=0;i<s;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.print("Enter number to search: ");
+        int e = sc.nextInt();
+        int count=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]==e){
+                count++;
+            }
+        }
+        if(count>0){
+            System.out.print("Element found");
+        }
+        else{
+            System.out.print("Element not found");
+        }
+    }
+}
+/************************OUTPUT****************************
+ Enter your array size: 6
+ Enter Array Elemets:  4 8 9 1 3 2
+ Enter number to search: 3
+ Element found
+ *********************************************************/
